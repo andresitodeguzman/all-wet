02c88ac9303d0c37cb917be9280092899745f367
Andresito de Guzman <deguzman.andresito@gmail.com>
2018-03-01 19:24:59 +0800

    Added src

---
+++ src/_system/config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+All Wet
+2018
+
+Config File
+*/
+
+$app_name = "";
+$app_description = "";
+
+$primary_color = "";
+$accent_color = "";
+
+$primary_hex = "";
+$secondary_hex = "";
+?>
---
+++ src/app/index.php
@@ -0,0 +1,21 @@
+<?php
+/*
+All Wet 
+2018
+
+App Shell
+*/
+
+require_once("../_system/config.php");
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php require_once("../_system/head.php"); ?>
+</head>
+<body>
+
+</body>
+</html>
