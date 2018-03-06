9525eacbafe68209697de4cf336252f3a72c1646
Andresito de Guzman <deguzman.andresito@gmail.com>
2018-03-06 11:55:55 +0800

    updates

--- src/app/index.php
+++ src/app/index.php
@@ -13,10 +13,75 @@ require_once("../_system/config.php");
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>All Wet</title>
 	<?php require_once("../_system/head.php"); ?>
+	<script type="text/javascript" src="/app/_app.js"></script>
+	<style>
+        .nav-wrapper{
+            padding-left:1%;
+            padding-right: 3%;
+        }
+        .title {
+            padding-left: 1%;
+            font-size: 18pt;
+        }
+    </style>
 </head>
-<body>
+<body class="grey lighten-4">
+	<div class="navbar-fixed">
+        <nav class="blue darken-3">
+            <div class="nav-wrapper">
+            <a href="#" data-target="snav" class="show-on-large sidenav-trigger"><i class="material-icons">menu</i></a>
+                <a class="title" href="#"><b>All Wet</b></a>
+            </div>
+        </nav>
+    </div>
 
+	<ul class="sidenav" id="snav">
+        <li>
+            <div class="user-view">
+                <div class="background blue darken-2">
+                    <!--img src="/assets/imgs/sidenavbg.jpg"-->
+                </div>
+                <a href="app/profile.php">
+                    <span class="white-text name">
+                        <b>All Wet Customer</b>
+                    </span>
+                </a>
+                <a href="/app">
+                    <span class="white-text email">
+                        <?=$subscriber_number?>
+                    </span>
+                </a>
+            </div>
+        </li>
+		<div id="app">
+
+		<sidebar-entry
+			loc="/"
+			icon="add"
+			title="Hello World!"></sidebar-entry>
+
+		</div>
+	    <li><a href="/app/order.php"><i class="material-icons">add</i> Order</a></li>
+        <li><a href="/app"><i class="material-icons">list</i> My Order</a></li>
+        <li><a href="/authentication/logout.php"><i class="material-icons">person</i> Log-out</a></li>
+    </ul>
+
+</div>
 </body>
 </html>
+<script>
+new Vue({
+    el:"#app"
+});
+
+Vue.component('sidebar-entry',{
+    props:['loc','icon','title'],
+    template:`
+        <li>
+            <a href='{{loc}}'><i class='material-icons'>{{icon}}</i> {{title}}</a>
+        </li>
+    `
+});	
+</script>
--- src/index.php
+++ src/index.php
@@ -12,13 +12,76 @@ require_once("_system/config.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Document</title>
-    <?php
-    require_once("_system/head.php");
-    ?>
+    <title><?=$app_name?></title>
+    <?php require_once("_system/head.php"); ?>
+    <style>
+        html,body{
+            height: 100%;
+        }
+        .parallax-container {
+            height: 100%;
+        }
+    </style>
 </head>
 <body>
-<nav class="blue"></nav>
-<br><br>
+    <div class="parallax-container">
+        <div class="parallax">
+            <img src="/assets/images/heroimg.jpg">
+        </div>
+        <div class="container"><br><br>
+            <h2 class="white-text"><b>All Wet</b></h2>
+            <h5 class="white-text">
+                Purified Drinking <a href="/manage" class="white-text">Water</a>
+            </h5><br><br>
+            <div id="button"></div>
+        </div>
+    </div>
 </body>
 </html>
+<script>
+  $(document).ready(()=>{
+    $('.parallax').parallax();
+ 
+    setButton();
+    $(".btn").hide();
+    $(".btn").fadeIn();
+  });
+
+  var checkLoginStatus = ()=>{
+      if(localStorage.getItem('all-wet-login')){
+          var login = localStorage.getItem('all-wet-login');
+          if(login == true){
+              return true;
+          } else {
+              return false;
+          }
+      } else {
+          localStorage.setItem("all-wet-login",false);
+          return false;
+      }
+  };
+
+  var setButton = ()=>{
+
+    let status = checkLoginStatus();
+
+    let loginButton = `
+        <a class="btn btn-large blue darken-4 waves-effect waves-light" href="/authenticate">
+            Sign-In with Your Mobile Number
+        </a>`;
+    
+    let appButton = `
+        <a class="btn btn-large blue darken-4 waves-effect waves-light" href="/app">
+            Open App
+        </a>`;
+        
+
+      if(status == true){
+          $("#button").html(appButton);
+      } else {
+          $("#button").html(loginButton);
+      }
+
+  };
+
+</script>
