eb4d22d892f9e2bfa4c47cd3d8dc646d04982f32
Andresito de Guzman <deguzman.andresito@gmail.com>
2018-03-10 10:22:52 +0800

    Class category

--- src/api/category/getCategories.php
+++ src/api/category/getCategories.php
@@ -12,14 +12,14 @@ require_once("../_boot.php");
 
 $category = new AllWet\Category($mysqli);
 
-$categories = $category->getCategories();
+$categories = $category->update(1, "Andresito", "de Guzman", "Haha");
 
-if(empty($categories)){
+/*if(empty($categories)){
     $categories = json_encode(array());
 } else {
     $categories = json_encode($categories);
 }
-
+*/
 echo $categories;
 
 ?>
--- src/class/AllWet/Category.class.php
+++ src/class/AllWet/Category.class.php
@@ -33,28 +33,165 @@
     }
 
     /**
-     * getCategories
+     * codeExists
+     * 
+     * @param: $category_code
+     * @return: Bool
+     */
+    private function codeExists($category_code){
+        $this->category_code = $category_code;
+
+        // Query if Category Code already exists
+        $stmt = $this->mysqli->prepare("SELECT category_id FROM `category` WHERE category_code = ? LIMIT 1");
+        $stmt->bind_param("s", $this->category_code);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        if($result->fetch_assoc()){
+            return True;
+        } else {
+            return False;
+        }
+
+    }
+
+    /**
+     * getAll
      * @param: none
      * @return: array
      */
-    public function getCategories(){
-        $cats = $this->mysqli->query("SELECT * FROM category");
+    public function getAll(){
+
+        // Query
+        $stmt = $this->mysqli->prepare("SELECT * FROM category");
+        $stmt->execute();
+        $cats = $stmt->get_result();
+
+        // Create placeholder array
         $this->category_array = array();
+
+        // Loop along results
         while($cat = $cats->fetch_array()){
+
             $category_id = $cat['category_id'];
             $category_name = $cat['category_name'];
             $category_description = $cat['category_description'];
             $category_code = $cat['category_code'];
+
             $prep_arr = array(
                 "category_id"=>$category_id,
                 "category_name"=>$category_name,
                 "category_description"=>$category_description,
                 "category_code"=>$category_code
             );
+
+            // Push to placeholder array
             array_push($this->category_array,$prep_arr);
         }
+
+        // Return Result
         return $this->category_array;
     }
+
+    /**
+     * get
+     * @param: $category_id
+     * @return: array
+    */
+    public function get($category_id){
+        // Handle Param
+        $this->category_id = $category_id;
+
+        // Prepare Statement
+        $stmt = $this->mysqli->prepare("SELECT * FROM `category` WHERE `category_id` = ?");
+        // Bind and execute
+        $stmt->bind_param("s",$this->category_id);
+        $stmt->execute();
+
+        // Handle Result
+        $result = $stmt->get_result();
+
+        // Return Result
+        return $result->fetch_assoc();
+    }
+
+    /**
+     * add
+     * @param: $category_name, $category_description, $category_code
+     * @return: Bool
+     */
+    public function add(String $category_name, String $category_description, String $category_code){
+        // Handle Params
+        $this->category_name = $category_name;
+        $this->category_description = $category_description;
+        $this->category_code = $category_code;
+        
+        if($this->codeExists($this->category_code)){
+            return False;
+        } else {
+            $stmt = $this->mysqli->prepare("INSERT INTO `category`(category_name, category_description, category_code) VALUES (?,?,?)");
+
+            $stmt->bind_param("sss",$this->category_name, $this->category_description, $this->category_code);
+            $stmt->execute();
+
+            return True;
+        }
+    }
+
+    /**
+     * delete
+     * 
+     * @param: $category_id
+     * @return: Bool
+     */
+    public function delete($category_id){
+        $this->category_id = $category_id;
+
+        $stmt = $this->mysqli->prepare("DELETE FROM `category` WHERE category_id = ?");
+        $stmt->bind_param("s",$this->category_id);
+        $stmt->execute();
+
+        $stmt = $this->mysqli->prepare("SELECT category_id FROM `category` WHERE category_id = ? LIMIT 1");
+        $stmt->bind_param("s",$this->category_id);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $result = $result->fetch_assoc();
+
+        if(empty($result)){
+            return True;
+        } else {
+            return False;
+        }
+
+    }
+
+    /**
+     * update
+     * 
+     * @param: $category_id, $category_name, $category_description, $category_code
+     * @return: Bool
+     */
+    public function update($category_id, String $category_name, String $category_description, String $category_code){
+        $this->category_id = $category_id;
+        $this->category_name = $category_name;
+        $this->category_description = $category_description;
+        $this->category_code = $category_code;
+
+        if($this->get($this->category_id)){
+            $stmt = $this->mysqli->prepare("UPDATE `category` SET `category_name` = ?, `category_description` = ?, `category_code` = ? WHERE `category_id` = ?");
+            $stmt->bind_param("ssss", $this->category_name, $this->category_description, $this->category_code, $this->category_id);
+            $stmt->execute();
+
+            return True;
+
+        } else {
+            return False;
+        }
+
+    }
+    
 
  }
 ?>
