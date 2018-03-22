90b3f2e1cf62938a9c0f80a15cf1dbd5d65a0198
deguzman.andresito <deguzman.andresito@gmail.com>
2018-03-22 01:39:12 -0400

    Edit Admin class

--- src/class/AllWet/Admin.class.php
+++ src/class/AllWet/Admin.class.php
@@ -14,10 +14,11 @@
     // Properties
     private $mysqli;
 
-    public $admin_id;
-    public $admin_name;
-    public $admin_password;
-    public $admin_image;
+    private $admin_id;
+    private $admin_name;
+    private $admin_username;
+    private $admin_password;
+    private $admin_image;
 
     // Methods
  
@@ -26,9 +27,58 @@
      * @param: 
      * @return: void
      */
-    function __construct(){
-
+    function __construct($mysqli){
+      $this->mysqli = $mysqli;
     }
+   
+   final private function getAll(){
+     $stmt = $this->mysqli->prepare("SELECT `admin_id`, `admin_name`, `admin_username`, `admin_image` FROM `admin`");
+     $stmt->execute();
+     
+     $result = $stmt->get_result();
+     return $result->fetch_array();
+   }
+   
+   final private function get($admin_id){
+     $this->admin_id = $admin_id;
+     
+     $stmt = $this->mysqli->prepare("SELECT `admin_id`, `admin_name`, `admin_username`, `admin_image` FROM `admin` WHERE `admin_id` = ? LIMIT 1");
+     $stmt->bind_param("s", $this->admin_id);
+     $stmt->execute();
+     
+     $result = $stmt->get_result();
+     if($result->fetch_assoc()){
+       return $result->fetch_assoc();       
+     } else {
+       return False;
+     }
+
+   }
+   
+   final private function getByUsername($admin_username){
+     $this->username = $admin_username;
+     
+     $stmt = $this->mysqli->prepare("SELECT `admin_id`, `admin_name`, `admin_username`, `admin_image` FROM `admin` WHERE `admin_username` = ? LIMIT 1");
+     $stmt->bind_param("s", $this->admin_username);
+     $stmt->execute();
+     
+     $result = $stmt->get_result();
+     return $result->fetch_assoc();
+   }
+   
+   final private function delete($admin_id){
+     $this->admin_id = $admin_id;
+     
+     $stmt = $this->mysqli->prepare("DELETE FROM `admin` WHERE `admin_username` = ?");
+     $stmt->bind_param("s", $this->admin_id);
+     $stmt->execute();
+     
+     if($this->get($this->admin_id)){
+       return False;
+     } else {
+       return True;       
+     }
+   }
 
 }
 ?>
