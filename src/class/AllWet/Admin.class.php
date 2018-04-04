548ab61d2b08ee71a45f0cf4959f4da690b32812
Andresito de Guzman <deguzman.andresito@gmail.com>
2018-04-04 14:32:45 +0800

    Added add method in AllWet\Admin

--- src/class/AllWet/Admin.class.php
+++ src/class/AllWet/Admin.class.php
@@ -12,8 +12,8 @@
  class Admin {
 
     // Properties
-    public $mysqli;
-
+    public $mysqli; 
+  
     public $admin_id;
     public $admin_name;
     public $admin_username;
@@ -28,9 +28,48 @@
      * @return: void
      */
     function __construct($mysqli){
-      $this->mysqli = $mysqli;
+      $this->mysqli = $mysqli;0
     }
-   
+  
+   final public function add(Array $admin_array){
+    if($admin_array['admin_name']) $this->admin_name = $admin_array['admin_name'];
+    if($admin_array['admin_username']) $this->admin_username = $admin_array['admin_username'];
+    if($admin_array['admin_password']) $this->admin_password = $admin_array['admin_password'];
+    if($admin_array['admin_image']) $this->admin_image = $admin_array['admin_image'];
+
+    if($this->usernameExists($this->username)){
+     return "Username already exists";
+    } else {
+     if(strlen($this->admin_password)<8){
+       return "Password less than 8 characters";
+      } else {
+        $this->admin_password = password_hash($this->admin_password, PASSWORD_DEFAULT);
+        
+        $stmt = $this->mysqli->prepare("INSERT INTO `admin` (admin_name,admin_username,admin_password,admin_image) VALUES (?,?,?,?)");
+        $stmt->bind_param("s,s,s,s",$this->admin_name, $this->admin_username, $this->admin_password, $this->admin_image);
+        $stmt->execute();
+
+        return True;
+
+     }
+    }
+   }
+  
+   final public function usernameExists(String $admin_username){
+    $this->admin_username = $admin_username;
+    $stmt = $this->mysqli->prepare("SELECT `admin_id` FROM `admin` WHERE `admin_username` = ? LIMIT 1");
+    $stmt->bind_param("s", $this->admin_username);
+    $stmt->execute();
+    
+    $result = $stmt->get_result();
+    $result = $result->fetch_assoc();
+    if($result){
+     return True;
+    } else {
+     return False;
+    }
+   }
+  
    final public function getAll(){
      $stmt = $this->mysqli->prepare("SELECT `admin_id`, `admin_name`, `admin_username`, `admin_image` FROM `admin`");
      $stmt->execute();
