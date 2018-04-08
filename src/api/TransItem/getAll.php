25ca7d3e87e348d304cb55de2c8695ef5de91b96
kevin mendoza <kevin_rm05@yahoo.com>
2018-04-08 11:50:27 +0800

    2edit

--- src/api/TransItem/getAll.php
+++ src/api/TransItem/getAll.php
@@ -15,7 +15,19 @@ require_once("../_boot.php");
 $obj = new AllWet\TransItem($mysqli);
 
 $data = $obj->getAll();
-
+if(empty($data)){
+    $t_array = array();
+    
+    foreach($data as $trans){
+    $product_name = $trans['product_name'];
+        
+        $a_arr = array(
+        "product_name"=>"$product_name",
+                
+        );
+        array_push($t_array,$a_arr);
+    }
+}	
 
 if(empty($data)){
     $data = json_encode(array());
